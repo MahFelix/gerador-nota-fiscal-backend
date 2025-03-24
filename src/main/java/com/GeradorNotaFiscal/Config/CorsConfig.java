dd9df3053a71bff93f63f15aa1090570422f83cf
AgFelix <marcelo12543@hotmail.com>
2025-03-24 12:15:54 -0300

    ✨ feat:  Add CorsConfig

---
+++ src/main/java/com/GeradorNotaFiscal/Config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.GeradorNotaFiscal.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permite todos os endpoints
+                        .allowedOriginPatterns("http://localhost:5173") // Usa allowedOriginPatterns para domínios específicos
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                        .allowedHeaders("*") // Todos os headers permitidos
+                        .allowCredentials(true); // Permite envio de cookies
+            }
+        };
+    }
+}
