f76d9331d958a38af9f90d896491e93c1b4149cc
AgFelix <marcelo12543@hotmail.com>
2025-03-24 12:15:47 -0300

    ✨ feat:  Add GNF Class

---
+++ src/main/java/com/GeradorNotaFiscal/GeradorNotaFiscalApplication.java
@@ -0,0 +1,13 @@
+package com.GeradorNotaFiscal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorNotaFiscalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorNotaFiscalApplication.class, args);
+	}
+
+}
